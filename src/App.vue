59dfdb00cb9de19dad56f228c04012b9faccb581
denispolovyan <denispolovyan@gmail.com>
2023-09-15 20:09:36 +0300

    feature: created simple header

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,108 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+   <div class="wrapper ">
+      <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-5">
+         <img class="h-20" src="./assets/Logo.png" alt="Logo" />
+			<div class="flex gap-1 items-center font-semibold" v-if="false">
+				<a href="#" class="">All done</a>
+				<CheckCircleIcon class="w-5"/>
+			</div>
+			<div class="flex gap-1 items-center font-semibold" v-else>
+				<a href="#" class="">Progress</a>
+				<p>20%</p>
+				<p class="w-4 h-4 bg-red-600 rounded-full"></p>
+			</div>
+      </header>
+      <main class="px-5 py-2">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni recusandae eos cupiditate
+         repellendus distinctio voluptas. Nisi quia ea maxime libero nulla corporis error,
+         perspiciatis expedita sint earum omnis tempora eius repellendus exercitationem hic, dolorum
+         nam tenetur eligendi non. Pariatur incidunt nulla placeat dicta cum ducimus fuga sint
+         beatae fugit maxime expedita minima obcaecati debitis ab ut porro molestias tempore hic
+         accusantium cumque saepe asperiores vero, assumenda reprehenderit. Magnam neque sunt
+         voluptas officia id, laboriosam unde quisquam quasi velit in commodi non suscipit quo,
+         perferendis earum maiores ex reiciendis sapiente vel! Unde esse ut officiis at distinctio
+         neque quam, voluptas culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+         recusandae eos cupiditate repellendus distinctio voluptas. Nisi quia ea maxime libero nulla
+         corporis error, perspiciatis expedita sint earum omnis tempora eius repellendus
+         exercitationem hic, dolorum nam tenetur eligendi non. Pariatur incidunt nulla placeat dicta
+         cum ducimus fuga sint beatae fugit maxime expedita minima obcaecati debitis ab ut porro
+         molestias tempore hic accusantium cumque saepe asperiores vero, assumenda reprehenderit.
+         Magnam neque sunt voluptas officia id, laboriosam unde quisquam quasi velit in commodi non
+         suscipit quo, perferendis earum maiores ex reiciendis sapiente vel! Unde esse ut officiis
+         at distinctio neque quam, voluptas culpa! Lorem ipsum dolor sit amet consectetur
+         adipisicing elit. Magni recusandae eos cupiditate repellendus distinctio voluptas. Nisi
+         quia ea maxime libero nulla corporis error, perspiciatis expedita sint earum omnis tempora
+         eius repellendus exercitationem hic, dolorum nam tenetur eligendi non. Pariatur incidunt
+         nulla placeat dicta cum ducimus fuga sint beatae fugit maxime expedita minima obcaecati
+         debitis ab ut porro molestias tempore hic accusantium cumque saepe asperiores vero,
+         assumenda reprehenderit. Magnam neque sunt voluptas officia id, laboriosam unde quisquam
+         quasi velit in commodi non suscipit quo, perferendis earum maiores ex reiciendis sapiente
+         vel! Unde esse ut officiis at distinctio neque quam, voluptas culpa! Lorem ipsum dolor sit
+         amet consectetur adipisicing elit. Magni recusandae eos cupiditate repellendus distinctio
+         voluptas. Nisi quia ea maxime libero nulla corporis error, perspiciatis expedita sint earum
+         omnis tempora eius repellendus exercitationem hic, dolorum nam tenetur eligendi non.
+         Pariatur incidunt nulla placeat dicta cum ducimus fuga sint beatae fugit maxime expedita
+         minima obcaecati debitis ab ut porro molestias tempore hic accusantium cumque saepe
+         asperiores vero, assumenda reprehenderit. Magnam neque sunt voluptas officia id, laboriosam
+         unde quisquam quasi velit in commodi non suscipit quo, perferendis earum maiores ex
+         reiciendis sapiente vel! Unde esse ut officiis at distinctio neque quam, voluptas culpa!
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni recusandae eos cupiditate
+         repellendus distinctio voluptas. Nisi quia ea maxime libero nulla corporis error,
+         perspiciatis expedita sint earum omnis tempora eius repellendus exercitationem hic, dolorum
+         nam tenetur eligendi non. Pariatur incidunt nulla placeat dicta cum ducimus fuga sint
+         beatae fugit maxime expedita minima obcaecati debitis ab ut porro molestias tempore hic
+         accusantium cumque saepe asperiores vero, assumenda reprehenderit. Magnam neque sunt
+         voluptas officia id, laboriosam unde quisquam quasi velit in commodi non suscipit quo,
+         perferendis earum maiores ex reiciendis sapiente vel! Unde esse ut officiis at distinctio
+         neque quam, voluptas culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+         recusandae eos cupiditate repellendus distinctio voluptas. Nisi quia ea maxime libero nulla
+         corporis error, perspiciatis expedita sint earum omnis tempora eius repellendus
+         exercitationem hic, dolorum nam tenetur eligendi non. Pariatur incidunt nulla placeat dicta
+         cum ducimus fuga sint beatae fugit maxime expedita minima obcaecati debitis ab ut porro
+         molestias tempore hic accusantium cumque saepe asperiores vero, assumenda reprehenderit.
+         Magnam neque sunt voluptas officia id, laboriosam unde quisquam quasi velit in commodi non
+         suscipit quo, perferendis earum maiores ex reiciendis sapiente vel! Unde esse ut officiis
+         at distinctio neque quam, voluptas culpa! Lorem ipsum dolor sit amet consectetur
+         adipisicing elit. Magni recusandae eos cupiditate repellendus distinctio voluptas. Nisi
+         quia ea maxime libero nulla corporis error, perspiciatis expedita sint earum omnis tempora
+         eius repellendus exercitationem hic, dolorum nam tenetur eligendi non. Pariatur incidunt
+         nulla placeat dicta cum ducimus fuga sint beatae fugit maxime expedita minima obcaecati
+         debitis ab ut porro molestias tempore hic accusantium cumque saepe asperiores vero,
+         assumenda reprehenderit. Magnam neque sunt voluptas officia id, laboriosam unde quisquam
+         quasi velit in commodi non suscipit quo, perferendis earum maiores ex reiciendis sapiente
+         vel! Unde esse ut officiis at distinctio neque quam, voluptas culpa! Lorem ipsum dolor sit
+         amet consectetur adipisicing elit. Magni recusandae eos cupiditate repellendus distinctio
+         voluptas. Nisi quia ea maxime libero nulla corporis error, perspiciatis expedita sint earum
+         omnis tempora eius repellendus exercitationem hic, dolorum nam tenetur eligendi non.
+         Pariatur incidunt nulla placeat dicta cum ducimus fuga sint beatae fugit maxime expedita
+         minima obcaecati debitis ab ut porro molestias tempore hic accusantium cumque saepe
+         asperiores vero, assumenda reprehenderit. Magnam neque sunt voluptas officia id, laboriosam
+         unde quisquam quasi velit in commodi non suscipit quo, perferendis earum maiores ex
+         reiciendis sapiente vel! Unde esse ut officiis at distinctio neque quam, voluptas culpa!
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni recusandae eos cupiditate
+         repellendus distinctio voluptas. Nisi quia ea maxime libero nulla corporis error,
+         perspiciatis expedita sint earum omnis tempora eius repellendus exercitationem hic, dolorum
+         nam tenetur eligendi non. Pariatur incidunt nulla placeat dicta cum ducimus fuga sint
+         beatae fugit maxime expedita minima obcaecati debitis ab ut porro molestias tempore hic
+         accusantium cumque saepe asperiores vero, assumenda reprehenderit. Magnam neque sunt
+         voluptas officia id, laboriosam unde quisquam quasi velit in commodi non suscipit quo,
+         perferendis earum maiores ex reiciendis sapiente vel! Unde esse ut officiis at distinctio
+         neque quam, voluptas culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+         recusandae eos cupiditate repellendus distinctio voluptas. Nisi quia ea maxime libero nulla
+         corporis error, perspiciatis expedita sint earum omnis tempora eius repellendus
+         exercitationem hic, dolorum nam tenetur eligendi non. Pariatur incidunt nulla placeat dicta
+         cum ducimus fuga sint beatae fugit maxime expedita minima obcaecati debitis ab ut porro
+         molestias tempore hic accusantium cumque saepe asperiores vero, assumenda reprehenderit.
+         Magnam neque sunt voluptas officia id, laboriosam unde quisquam quasi velit in commodi non
+         suscipit quo, perferendis earum maiores ex reiciendis sapiente vel! Unde esse ut officiis
+         at distinctio neque quam, voluptas culpa!
+      </main>
+   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style></style>
