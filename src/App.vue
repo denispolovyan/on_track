a6a4e99bd7dfa54c7a59a6c2fb9147320c73a320
denispolovyan <denispolovyan@gmail.com>
2023-09-29 15:51:53 +0300

    feature: added simple adaptive version for laptops/pc

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@ import HeaderPanel from './components/HeaderPanel.vue'
 import NavigationListVue from './components/NavigationList.vue'
 import MainContent from './components/MainContent.vue'
 
+const styles = 'mx-auto start:w-full lg:w-2/3 xl:w-1/2 2xl:w-2/5 3xl:w-1/3'
 </script>
 
 <template>
-   <header-panel/>
-   <main-content />
-   <navigation-list-vue />
+      <header-panel :class="styles"/>
+      <main-content :class="styles"/>
+      <navigation-list-vue :class="styles"/>
 </template>
 
 <style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-	"./index.html",
-	"./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+   plugins: [],
+   theme: {
+      screens: {
+			'start': '0px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+			'3xl': '1792px',
+      }
+   }
 }
-
